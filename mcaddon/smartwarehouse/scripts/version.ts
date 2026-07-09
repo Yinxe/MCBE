0fdbb85962432cc4c857c9beaa607138a314034d
Yinx <1423716216@qq.com>
2026-07-09 17:22:06 +0800

    v0.0.55 修复装备分类失败的问题

--- mcaddon/smartwarehouse/scripts/version.ts
+++ mcaddon/smartwarehouse/scripts/version.ts
@@ -1,5 +1,5 @@
 // 此文件由 just.config.ts 在构建时自动生成
 
 export const VERSION = "0.0.55";
-export const BUILD_TIME = "2026-07-09T07:39:15.964Z";
+export const BUILD_TIME = "2026-07-09T07:40:19.984Z";
 export const PROJECT_URL = "https://github.com/YinxSmartHouse/SmartWarehouse";
