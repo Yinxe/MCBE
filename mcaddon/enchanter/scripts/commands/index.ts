8f3ea5d8d66db16ed9197ac673b0f6d5a8128764
Yinx <1423716216@qq.com>
2026-07-27 15:53:41 +0400

    chore: remove enchanter module

--- mcaddon/enchanter/scripts/commands/index.ts
+++
@@ -1,7 +0,0 @@
-import { StartupEvent } from "@minecraft/server";
-import { registerMenuCommand } from "./menu";
-
-export function registerAllCommands(event: StartupEvent): void {
-  const registry = event.customCommandRegistry;
-  registerMenuCommand(registry);
-}
