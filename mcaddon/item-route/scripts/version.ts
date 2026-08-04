2d0c5d8ac1b170a15a76b4b771f17c9dcf1c10cc
Yinx <1423716216@qq.com>
2026-08-04 20:07:03 +0400

    item-route: 同步构建产物 version.ts

--- mcaddon/item-route/scripts/version.ts
+++ mcaddon/item-route/scripts/version.ts
@@ -1,5 +1,5 @@
 // 此文件由 just.config.ts 在构建时自动生成
 
 export const VERSION = "0.1.0";
-export const BUILD_TIME = "2026-08-04T12:36:13.738Z";
+export const BUILD_TIME = "2026-08-04T15:59:59.077Z";
 export const PROJECT_URL = "https://github.com/YinxSmartHouse/item-route";
