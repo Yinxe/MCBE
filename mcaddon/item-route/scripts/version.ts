9e5309a43747b5c661401e0711c2cd983c20eb86
Yinx <1423716216@qq.com>
2026-08-09 12:47:43 +0800

    item-route@0.1.17: 容器补注册简单方案 + .name 安全化 + 活塞失联容器修复 + 潜影盒防套娃 + hasItemType O(1) + 物品族修正

--- mcaddon/item-route/scripts/version.ts
+++ mcaddon/item-route/scripts/version.ts
@@ -1,5 +1,5 @@
 // 此文件由 just.config.ts 在构建时自动生成
 
-export const VERSION = "0.1.16";
-export const BUILD_TIME = "2026-08-09T04:10:13.162Z";
+export const VERSION = "0.1.17";
+export const BUILD_TIME = "2026-08-09T04:45:21.403Z";
 export const PROJECT_URL = "https://github.com/YinxSmartHouse/item-route";
